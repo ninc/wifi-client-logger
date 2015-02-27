7484a1a1a768d505fc4d9f8532c9b8229297324b
Jacob Sundqvist <jacsu246@gmail.com>
2015-02-27 18:21:27 +0100

    fixed minor issues

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-
 # SETUP MONITOR MODE:
 # http://pharos.ece.utexas.edu/wiki/index.php/How_to_Measure_the_Received_Signal_Strength_of_WiFi_Beacons
 
@@ -8,11 +7,6 @@
 # sudo ifconfig moni0 up
 
 import fileinput
-
-
-
-# 2015-02-27 16:35:44.828071 380232742us tsft 1.0 Mb/s 2412 MHz 11g -53dB signal [bit 29] Retry 314us BSSID:00:22:b0:ab:d8:3a DA:44:74:6c:52:ab:e0 SA:00:22:b0:ab:d8:3a Probe Response (Stora Visatter) [1.0* 2.0* 5.5* 11.0* Mbit] CH: 6, PRIVACY
-
 
 def find_channel(line):
     mhz = "MHz"
@@ -28,7 +22,6 @@ def get_rss(line):
         return line[rss_exists-3:rss_exists+len(db)]
     return ""
 
-
 def find_probe(line):
     probe = "Probe"
     probe_req = line.find(probe.upper())
@@ -42,7 +35,6 @@ def find_probe_request(line):
     if(probe_req >= 0):
         return True
     return False
-
 
 def find_mac_source(line, mac):
     source = "SA:" + mac
@@ -77,25 +69,16 @@ def print_data(line):
     
     print(date + " " + time + " " + rss + " " + mac_source + " " + mac_dest)
 
-
-
-
 def main():
     
-    ninc_g3_mac = "2C:54:CF:FB:4C:C8"
+    target_mac = "2C:54:CF:FB:4C:C8"
 
     try:
         while(True):
             for line in fileinput.input():
                 l = line.upper()
-                channel = find_channel(l)
-                if(channel != "2412" and channel != ""):
-                    print(channel)
-
-                # if(find_mac_source(l, ninc_g3_mac)):
-                    # print_data(l)
-                    # print(l)
-                   # return
+                if(find_mac_source(l, target_mac)):
+                    print(l)
     except KeyboardInterrupt:
         print("Program stopped by user")
 
